84a828ef65c57052dea4c5e9b645cd7d922f6a71
itmrrobot <nhannguyentri05@gmail.com>
2024-01-15 16:16:06 +0700

    Delete all code on branch

--- src/utils/index.tsx
+++
@@ -1,7 +0,0 @@
-export const formatCompactNumber = (number:number) => {
-    if (number < 1000) {
-      return number;
-    } else if (number >= 1000 && number < 1_000_000) {
-      return (number / 1000).toFixed(1) + "k";
-    } 
-  }
